87ebf10d6efc57e6b1760af93ab1aade2b10d193
tarcisiodesales-lang <tarcisiodesales@gmail.com>
2026-01-31 07:36:37 -0300

    criando arquivos php

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livrando-se da Maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30E}";
+        
+        ?>
+    </h1>
+    <p>Estou me livrando da Maldição.</p>
+</body>
+</html>
