7c9735e22d7688694837a0b39a8a7a48a034ea82
tarcisiodesales-lang <tarcisiodesales@gmail.com>
2026-02-15 21:46:48 -0300

    novas aulas

--- obtendo-dados-formulario/cad.php
+++ obtendo-dados-formulario/cad.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>resultado</title>
+    <link rel="stylesheet" href="estilo/style-guanabara.css">
     
 </head>
 <body>
-      
+      <header><h1>Resultado do processamento</h1></header>
+      <main>
+        <?php 
+        
+            $nome = $_GET["nome"] ?? "sem nome"; // ?? operador de null coalescing: se não enviar nada já recebe valor
+            $sobrenome = $_GET["sobrenome"] ?? "desconhecido";
+            echo "<p>É um prazer te conhecer, $nome $sobrenome! Esse é o meu site.";
+        
+        
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior.</a></p>
+      </main>
 </body>
 </html>
